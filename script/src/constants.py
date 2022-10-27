54dcee55a3ccd798aef8771bea2aed59df360f4b
AspireWebFX <philipxpromotes0@gmail.com>
2022-10-27 16:24:40 +0800

    yewsasd

--- script/src/constants.py
+++ script/src/constants.py
@@ -1,10 +1,10 @@
 import os
 
-MODE = "dev"
-if MODE == "dev":
+MODE = "mainnet"
+if MODE == "mainnet":
     NODE_URL = os.getenv("APTOS_NODE_URL", "https://fullnode.devnet.aptoslabs.com/v1")
     FAUCET_URL = os.getenv("APTOS_FAUCET_URL", "https://faucet.devnet.aptoslabs.com")
-elif MODE == "test":
+elif MODE == "mainnet":
     NODE_URL = os.getenv("APTOS_NODE_URL", "https://fullnode.testnet.aptoslabs.com/v1")
     FAUCET_URL = os.getenv("APTOS_FAUCET_URL", "https://faucet.testnet.aptoslabs.com")
 else:
