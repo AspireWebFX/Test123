c3b924b6a99c10a187ef943236b92ad8c8c942de
AspireWebFX <philipxpromotes0@gmail.com>
2022-10-27 16:55:14 +0800

    asdasd

--- mint-site/helpers/candyMachineInfo.js
+++ mint-site/helpers/candyMachineInfo.js
@@ -1,4 +1,4 @@
-export const candyMachineAddress = "0x8cbc1d0265706a924399d6ccbd3b1a611313d6a512cc5b9109313d6d6da2635a";
+export const candyMachineAddress = "0x676e4d9cbc466195cc643299e4f621e84f48125b6922325cb93471661a47dc71";
 export const collectionName = "Test123"; // Case sensitive!
 export const collectionCoverUrl = "https://cloudflare-ipfs.com/ipfs/QmbNB4GzfpZJzMPw2qS5DEqbdnBJcmYDESPKA6bWyH1Dcm";
 export const mode = "mainnet"; // "dev" or "test" or "mainnet"
@@ -7,7 +7,7 @@ export let NODE_URL;
 export const CONTRACT_ADDRESS = "0x481efbf0c3cbec627b5f5674287d4ae6ee770da5949dcfe698a8520108236a33";
 export const COLLECTION_SIZE = 10
 let FAUCET_URL;
-if (mode == "mainnet") {
+if (mode == "dev") {
     NODE_URL = "https://fullnode.devnet.aptoslabs.com/v1";
     FAUCET_URL = "https://faucet.devnet.aptoslabs.com";
 } else if (mode === "test") {
